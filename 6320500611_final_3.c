295ecf18d18a38dcf27f29d8da1843867b37c73d
ChargeBrX <panuwat.janj@ku.th>
2021-03-29 17:14:59 +0700

    No Magic Square

---
+++ 6320500611_final_3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int main()
+{
+    int n,m,o,i,j,k;
+
+    scanf("%d %d %d",&n,&m,&o);
+
+    int a[n][n],b[m][m],c[o][o];
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&c[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+}
